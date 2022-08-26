1b864f413d0dfc8ad3fc611712b8bbc03e200952
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-08-26 00:42:38 -0300

    refactor(Token/Bcrypt): Refatorado as funções de geração de token e hash do bcrypt

--- back-end/src/api/api.ts
+++ back-end/src/api/api.ts
@@ -1,7 +1,14 @@
+import 'express-async-errors';
 import express from 'express';
+import errorHandle from '../errors/errorHandle';
+import userRouter from '../routers/userRouter';
 
 const api = express();
 
 api.use(express.json());
+
+api.use('/users', userRouter);
+
+api.use(errorHandle);
 
 export default api;
---
+++ back-end/src/utils/token.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from 'fs';
+import jwt from 'jsonwebtoken';
+
+const SECRET_KEY = readFileSync('/.private.key');
+
+export const createToken = (user: object | boolean) => {
+  return jwt.sign({ user }, SECRET_KEY, {
+    expiresIn: '1d',
+    algorithm: 'HS256',
+  });
+};
+
+export const decoderToken = (token: string) => {
+  return jwt.verify(token, SECRET_KEY);
+};
