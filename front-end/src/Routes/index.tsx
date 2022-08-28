85c900b4aea414e88699813f2ffa4733ae84fc0d
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-08-27 21:33:57 -0300

    feat(Routes): implementado o componente de rotas

--- front-end/src/Routes/index.tsx
+++ front-end/src/Routes/index.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Login from '../Pages/Login';
+import LoginRegister from '../Pages/LoginRegister';
 import Home from '../Pages/Home';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<LoginRegister />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );
